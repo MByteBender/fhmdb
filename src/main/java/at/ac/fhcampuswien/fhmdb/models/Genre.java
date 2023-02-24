69be0e643411a4614c92dc2a3cde925647efe111
tristan <tristanwestreicher@gmail.com>
2023-02-24 17:41:08 +0100

    sort ascending with lambda expression

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Genre.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Genre.java
@@ -1,41 +1,24 @@
 package at.ac.fhcampuswien.fhmdb.models;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class Genre {
-    private String genreName;
-
-    public Genre(String genreName) {
-        this.genreName = genreName;
-    }
-
-    public String getName() {
-        return genreName;
-    }
-
-    public static List<Genre> getDefaultGenres() {
-        List<Genre> defaultGenres = new ArrayList<>();
-        defaultGenres.add(new Genre("ACTION"));
-        defaultGenres.add(new Genre("ADVENTURE"));
-        defaultGenres.add(new Genre("ANIMATION"));
-        defaultGenres.add(new Genre("BIOGRAPHY"));
-        defaultGenres.add(new Genre("COMEDY"));
-        defaultGenres.add(new Genre("CRIME"));
-        defaultGenres.add(new Genre("DRAMA"));
-        defaultGenres.add(new Genre("DOCUMENTARY"));
-        defaultGenres.add(new Genre("FAMILY"));
-        defaultGenres.add(new Genre("FANTASY"));
-        defaultGenres.add(new Genre("HISTORY"));
-        defaultGenres.add(new Genre("HORROR"));
-        defaultGenres.add(new Genre("MUSICAL"));
-        defaultGenres.add(new Genre("MYSTERY"));
-        defaultGenres.add(new Genre("ROMANCE"));
-        defaultGenres.add(new Genre("SCIENCE_FICTION"));
-        defaultGenres.add(new Genre("SPORT"));
-        defaultGenres.add(new Genre("THRILLER"));
-        defaultGenres.add(new Genre("WAR"));
-        defaultGenres.add(new Genre("WESTERN"));
-        return defaultGenres;
-    }
+public enum Genre {
+    ACTION,
+    ADVENTURE,
+    ANIMATION,
+    BIOGRAPHY,
+    COMEDY,
+    CRIME,
+    DRAMA,
+    DOCUMENTARY,
+    FAMILY,
+    FANTASY,
+    HISTORY,
+    HORROR,
+    MUSICAL,
+    MYSTERY,
+    ROMANCE,
+    SCIENCE_FICTION,
+    SPORT,
+    THRILLER,
+    WAR,
+    WESTERN
 }
--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -30,23 +30,59 @@ public class Movie {
     }
 
     public static List<Movie> initializeMovies(){
-        List<Movie> movies = new ArrayList<>();
-        movies.add(new Movie("The Matrix", "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.", Arrays.asList(new Genre("ACTION"), new Genre("SCIENCE_FICTION"))));
-        movies.add(new Movie("Forrest Gump", "The presidencies of Kennedy and Johnson, the Vietnam War, and the Watergate scandal, is seen through the eyes of an Alabama man with an IQ of 75.", Arrays.asList(new Genre("DRAMA"), new Genre("ROMANCE"), new Genre("COMEDY"))));
-        movies.add(new Movie("Pulp Fiction", "The lives of two mob hitmen, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.", Arrays.asList(new Genre("CRIME"), new Genre("DRAMA"), new Genre("THRILLER"))));
-        movies.add(new Movie("Jurassic Park", "A pragmatic paleontologist visiting an almost complete theme park is tasked with protecting a couple of kids after a power failure causes the park's cloned dinosaurs to run loose.", Arrays.asList(new Genre("ADVENTURE"), new Genre("SCIENCE_FICTION"), new Genre("THRILLER"))));
-        movies.add(new Movie("The Silence of the Lambs", "A young F.B.I. cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer, a madman who skins his victims.", Arrays.asList(new Genre("CRIME"), new Genre("DRAMA"), new Genre("THRILLER"))));
-        movies.add(new Movie("The Lion King", "Lion prince Simba and his father are targeted by his bitter uncle, who wants to ascend the throne himself.", Arrays.asList(new Genre("ANIMATION"), new Genre("ADVENTURE"), new Genre("DRAMA"))));
-        movies.add(new Movie("Titanic", "A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.", Arrays.asList(new Genre("DRAMA"), new Genre("ROMANCE"), new Genre("HISTORY"))));
-        movies.add(new Movie("The Godfather", "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.", Arrays.asList(new Genre("DRAMA"), new Genre("CRIME"))));
-        movies.add(new Movie("The Dark Knight", "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.", Arrays.asList(new Genre("ACTION"), new Genre("CRIME"), new Genre("DRAMA"))));
-        movies.add(new Movie("The Lord of the Rings: The Fellowship of the Ring", "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.", Arrays.asList(new Genre("ACTION"), new Genre("ADVENTURE"), new Genre("FANTASY"))));
-        movies.add(new Movie("Inception", "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.", Arrays.asList(new Genre("ACTION"), new Genre("THRILLER"), new Genre("SCIENCE_FICTION"))));
-        movies.add(new Movie("The Shawshank Redemption", "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.", Arrays.asList(new Genre("DRAMA"), new Genre("CRIME"))));
-        movies.add(new Movie("Star Wars: Episode IV - A New Hope", "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station.", Arrays.asList(new Genre("ACTION"), new Genre("ADVENTURE"), new Genre("FANTASY"), new Genre("SCIENCE_FICTION"))));
-        movies.add(new Movie("The Avengers", "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.", Arrays.asList(new Genre("ACTION"), new Genre("ADVENTURE"), new Genre("SCIENCE_FICTION"))));
-        movies.add(new Movie("The Social Network", "Harvard student Mark Zuckerberg creates the social networking site that would become known as Facebook, but is later sued by two brothers who claimed he stole their idea, and the co-founder who was later squeezed out of the business.", Arrays.asList(new Genre("DRAMA"), new Genre("BIOGRAPHY"))));
-        movies.add(new Movie("The Grand Budapest Hotel", "The adventures of Gustave H, a legendary concierge at a famous hotel from the fictional Republic of Zubrowka between the first and second World Wars, and Zero Moustafa, the lobby boy who becomes his most trusted friend.", Arrays.asList(new Genre("COMEDY"), new Genre("ADVENTURE"))));
+        List<Movie>  movies= new ArrayList<>();
+        movies.add(new Movie(
+                "The Matrix",
+                "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+                Arrays.asList(Genre.ACTION, Genre.SCIENCE_FICTION)));
+        movies.add(new Movie(
+                "Forrest Gump",
+                "The presidencies of Kennedy and Johnson, the Vietnam War, and the Watergate scandal, is seen through the eyes of an Alabama man with an IQ of 75.",
+                Arrays.asList(Genre.DRAMA, Genre.ROMANCE, Genre.COMEDY)));
+        movies.add(new Movie(
+                "Pulp Fiction",
+                "The lives of two mob hitmen, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+                Arrays.asList(Genre.DRAMA, Genre.CRIME, Genre.THRILLER)));
+        movies.add(new Movie(
+                "Jurassic Park",
+                "A pragmatic paleontologist visiting an almost complete theme park is tasked with protecting a couple of kids after a power failure causes the park's cloned dinosaurs to run loose.",
+                Arrays.asList(Genre.ADVENTURE, Genre.SCIENCE_FICTION, Genre.THRILLER)));
+//        movies.add (new Movie(
+//                "The Silence of the Lambs",
+//                "A young F.B.I. cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer, a madman who skins his victims.", Arrays.asList(new Genre("CRIME"), new Genre("DRAMA"), new Genre("THRILLER"))));
+//        movies.add(new Movie(
+//                "The Lion King",
+//                "Lion prince Simba and his father are targeted by his bitter uncle, who wants to ascend the throne himself.", Arrays.asList(new Genre("ANIMATION"), new Genre("ADVENTURE"), new Genre("DRAMA"))));
+//        movies.add (new Movie(
+//                "Titanic",
+//                "A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.", Arrays.asList(new Genre("DRAMA"), new Genre("ROMANCE"), new Genre("HISTORY"))));
+//        movies.add(new Movie(
+//                "The Godfather",
+//                "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.", Arrays.asList(new Genre("DRAMA"), new Genre("CRIME"))));
+//        movies.add(new Movie(
+//                "The Dark Knight",
+//                "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.", Arrays.asList(new Genre("ACTION"), new Genre("CRIME"), new Genre("DRAMA"))));
+//        movies.add(new Movie(
+//                "The Lord of the Rings: The Fellowship of the Ring",
+//                "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.", Arrays.asList(new Genre("ACTION"), new Genre("ADVENTURE"), new Genre("FANTASY"))));
+//        movies.add(new Movie(
+//                "Inception",
+//                "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.", Arrays.asList(new Genre("ACTION"), new Genre("THRILLER"), new Genre("SCIENCE_FICTION"))));
+//        movies.add(new Movie(
+//                "The Shawshank Redemption",
+//                "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.", Arrays.asList(new Genre("DRAMA"), new Genre("CRIME"))));
+//        movies.add(new Movie(
+//                "Star Wars: Episode IV - A New Hope",
+//                "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station.", Arrays.asList(new Genre("ACTION"), new Genre("ADVENTURE"), new Genre("FANTASY"), new Genre("SCIENCE_FICTION"))));
+//        movies.add(new Movie(
+//                "The Avengers",
+//                "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.", Arrays.asList(new Genre("ACTION"), new Genre("ADVENTURE"), new Genre("SCIENCE_FICTION"))));
+//        movies.add(new Movie(
+//                "The Social Network",
+//                "Harvard student Mark Zuckerberg creates the social networking site that would become known as Facebook, but is later sued by two brothers who claimed he stole their idea, and the co-founder who was later squeezed out of the business.", Arrays.asList(new Genre("DRAMA"), new Genre("BIOGRAPHY"))));
+//        movies.add(new Movie(
+//                "The Grand Budapest Hotel",
+//                "The adventures of Gustave H, a legendary concierge at a famous hotel from the fictional Republic of Zubrowka between the first and second World Wars, and Zero Moustafa, the lobby boy who becomes his most trusted friend.", Arrays.asList(new Genre("COMEDY"), new Genre("ADVENTURE"))));
         return movies;
     }
 }
